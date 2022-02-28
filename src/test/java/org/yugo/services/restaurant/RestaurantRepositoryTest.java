0a279c0116a74a02967cdd4b27527c71b0cb9b0a
Yugo Sakamoto <yugoccp@gmail.com>
2022-02-28 02:22:29 -0300

    test: implement RestaurantRepositoryTest

---
+++ src/test/java/org/yugo/services/restaurant/RestaurantRepositoryTest.java
@@ -0,0 +1,30 @@
+package org.yugo.services.restaurant;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class RestaurantRepositoryTest {
+
+    private RestaurantRepository restaurantRepository;
+
+    @BeforeEach
+    public void init() {
+        restaurantRepository = new RestaurantRepository();
+    }
+
+    @Test
+    public void When_GetAllRestaurants_Should_ReturnValidList() {
+        var result = restaurantRepository.getAllRestaurants();
+
+        Assertions.assertTrue(result.size() > 0);
+        
+        result.forEach(restaurant -> {
+            Assertions.assertNotNull(restaurant.getName());
+            Assertions.assertNotNull(restaurant.getDistance());
+            Assertions.assertNotNull(restaurant.getPrice());
+            Assertions.assertNotNull(restaurant.getCuisine());
+            Assertions.assertNotNull(restaurant.getCustomerRating());
+        });
+    }
+}
