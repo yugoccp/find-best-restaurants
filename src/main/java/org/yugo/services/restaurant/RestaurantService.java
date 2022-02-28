9dda79a2c984d7aabb7c0f0b066c001fcaf41556
Yugo Sakamoto <yugoccp@gmail.com>
2022-02-27 23:45:33 -0300

    test: create test cases

---
+++ src/main/java/org/yugo/services/restaurant/RestaurantService.java
@@ -0,0 +1,15 @@
+package org.yugo.services.restaurant;
+
+import java.util.Collections;
+import java.util.List;
+
+public class RestaurantService {
+
+    public RestaurantService() {
+    }
+
+    public List<Restaurant> findBestRestaurant(RestaurantSearchParameter searchParameter) throws IllegalArgumentException {
+        return Collections.emptyList();
+    }
+
+}
---
+++ src/test/java/org/yugo/services/restaurant/RestaurantServiceTest.java
@@ -0,0 +1,72 @@
+package org.yugo.services.restaurant;
+
+import org.junit.jupiter.api.Test;
+
+public class RestaurantServiceTest {
+
+    
+    @Test
+    public void When_InvalidCustomerRating_Should_ReturnInvalidMessage() {
+       
+    }
+    
+    @Test
+    public void When_InvalidPrice_Should_ReturnInvalidMessage() {
+       
+    }
+
+    @Test
+    public void When_InvalidDistance_Should_ReturnInvalidMessage() {
+       
+    }
+
+    @Test
+    public void When_NoMatch_Should_ReturnEmptyList() {
+       
+    }
+
+    @Test
+    public void When_LessThan5Matches_Should_ReturnAll() {
+       
+    }
+
+    @Test
+    public void When_MoreThan5Matches_Should_Return5Matches() {
+       
+    }
+
+    @Test
+    public void When_Match_Should_ContainsName() {
+       
+    }
+
+    @Test
+    public void When_Match_Should_ReturnExactOrPartialhName() {
+
+    }
+
+    @Test
+    public void When_Match_Should_ReturnEqualOrMoreThanCustomerRating() {
+      
+    }
+
+    @Test
+    public void When_Match_Should_ReturnEqualOrLessThanPrice() {
+        
+    }
+
+    @Test
+    public void When_Match_Should_ReturnEqualOrLessThanDistance() {
+       
+    }
+
+    @Test
+    public void When_Match_Should_ReturnExactOrPartialCuisine() {
+        
+    }
+
+    @Test
+    public void When_MultipleMatches_Should_MatchOrder() {
+        
+    }
+}
